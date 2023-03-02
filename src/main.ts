2bc0f64cc6123fdc044a27c99a391aa44c03da33
Hryhorii Avcharov <h.avcharov@danieli.com>
2023-03-02 11:52:53 +0100

    Pushed sln chapter 9

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+import { environment } from './app/environments/environment';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
